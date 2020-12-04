0cf3dc0372ae825f0c0845da3b925704a608caf9
Brian Onyando <bryndox@gmail.com>
2020-12-04 21:52:17 +0300

    Beautified UI slightly

--- app/src/main/java/com/ubunifuconcepts/jsonplaceholder/MainActivity.kt
+++ app/src/main/java/com/ubunifuconcepts/jsonplaceholder/MainActivity.kt
@@ -6,9 +6,11 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.snackbar.Snackbar
 import com.ubunifuconcepts.jsonplaceholder.model.Post
 import com.ubunifuconcepts.jsonplaceholder.view.PostAdapter
 import com.ubunifuconcepts.jsonplaceholder.viewmodel.PostsViewModel
+
 
 class MainActivity : AppCompatActivity() {
     lateinit var postAdapter: PostAdapter
@@ -43,8 +45,22 @@ class MainActivity : AppCompatActivity() {
         postsViewModel = ViewModelProvider(this).get(PostsViewModel::class.java)
         postsViewModel.postsLiveData
             .observe(this, Observer<List<Post>> { posts ->
-                updatePostsList(posts)
+                if (posts.isNotEmpty()) {
+                    updatePostsList(posts)
+                } else {
+                    showDataNotLoadedMessage()
+                }
             })
+    }
+
+    private fun showDataNotLoadedMessage() {
+        Snackbar.make(
+            postsRecyclerView,
+            getString(R.string.data_not_loaded),
+            Snackbar.LENGTH_LONG
+        ).also { snackBar ->
+            snackBar.show()
+        }
     }
 
     private fun updatePostsList(posts: List<Post>) {
