471d6d54626e7fcc1634ab01623a29e6728ba8a4
Brian Onyando <bryndox@gmail.com>
2020-12-04 19:53:49 +0300

    Added Repository method for fetching posts using Retrofit

--- app/src/main/java/com/ubunifuconcepts/jsonplaceholder/repository/ItemRepository.kt
+++ app/src/main/java/com/ubunifuconcepts/jsonplaceholder/repository/ItemRepository.kt
@@ -1,4 +1,17 @@
 package com.ubunifuconcepts.jsonplaceholder.repository
 
+import android.util.Log
+import com.ubunifuconcepts.common.network.RestClient
+import com.ubunifuconcepts.jsonplaceholder.model.Post
+import com.ubunifuconcepts.jsonplaceholder.service.NetworkService
+
 object ItemRepository {
+    suspend fun loadPosts(): List<Post> {
+        return try {
+            RestClient.service(NetworkService::class.java).fetchPosts()
+        } catch (e: Exception) {
+            Log.e(javaClass.simpleName, "Error fetching posts: ${e.message}")
+            listOf()
+        }
+    }
 }
--- app/src/main/java/com/ubunifuconcepts/jsonplaceholder/service/NetworkService.kt
+++ app/src/main/java/com/ubunifuconcepts/jsonplaceholder/service/NetworkService.kt
@@ -5,5 +5,5 @@ import retrofit2.http.GET
 
 interface NetworkService {
     @GET("posts")
-    fun createUser(): List<Post>
+    suspend fun fetchPosts(): List<Post>
 }
